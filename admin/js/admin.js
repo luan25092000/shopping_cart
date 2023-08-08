6e94a499fb1a01fb1939fcab9a1bf3fff09d6aa4
luan25092000 <nguyenhuuluan17@gmail.com>
2023-08-08 21:35:00 +0700

    delete and update product

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,3 +1,6 @@
+document.getElementById('updateAndCancel').style.display = 'none';
+localStorage.removeItem('productId');
+
 if (localStorage.getItem('isLogin') === null || localStorage.getItem('isLogin') === 'undefined') {
     alert('Please login !');
     window.location.href = '/admin/login.html';
@@ -151,6 +154,151 @@ function deleteProduct(productId) {
         var filterProducts = products.filter(product => product.productId != productId);
         localStorage.setItem('products', JSON.stringify(filterProducts));
         alert('Delete Successfully');
+        window.location.href = 'admin.html';
+    }
+}
+
+var searchBtn = document.getElementById('searchBtn');
+if (typeof(searchBtn) != 'undefined' && searchBtn != null) {
+    searchBtn.addEventListener('click', function() {
+        var searchItem = document.getElementById('searchItem').value.toLowerCase();
+        if (searchItem.length === 0) {
+            alert('Please fill product name in search input');
+            return;
+        }
+        var products = JSON.parse(localStorage.getItem('products'));
+        // Kiểm tra tên sản phẩm có chứa từ khóa mình search không
+        var filterProducts = products.filter(product => product.productName.toLowerCase().indexOf(searchItem) > -1);
+        var html = `
+            <tr>
+                <th>ID Product</th>
+                
+                <th>Name</th>
+
+                <th>Price</th>
+
+                <th>Sale Price</th>
+
+                <th>Image</th>
+
+                <th>Action</th>
+            </tr>
+        `;
+        if (filterProducts.length > 0) { // có sản phẩm
+            for (var x of filterProducts) {
+                html += `
+                <tr>
+                    <td>${x.productId}</td>
+                    <td>${x.productName}</td>
+                    <td>
+                        ${x.productPrice.toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}
+                    </td>
+                    <td>
+                        ${x.productSalePrice > 0 ? x.productSalePrice.toLocaleString('it-IT', {style : 'currency', currency : 'VND'}) : ''}
+                    </td>
+                    <td>
+                        <img src="${x.productLink}" width="200">
+                    </td>
+                    <td>
+                        <button onclick="editProduct('${x.productId}')">Edit</button>
+                        <button onclick="deleteProduct('${x.productId}')">Delete</button>
+                    </td>
+                </tr>
+                `
+            }
+        } else {
+            html += '<td align="center" colspan="6">Không có sản phẩm nào</td>';
+        }
+    });
+}
+
+function editProduct(productId) {
+    var products = JSON.parse(localStorage.getItem('products'));
+    var productEdit = products.find(product => product.productId == productId);
+    document.getElementById('productName').value = productEdit.productName;
+    document.getElementById('productPrice').value = productEdit.productPrice;
+    document.getElementById('productSalePrice').value = productEdit.productSalePrice;
+    document.getElementById('productLink').value = productEdit.productLink;
+    document.getElementById('addBtn').style.display = 'none';
+    document.getElementById('updateAndCancel').style.display = 'block';
+    localStorage.setItem('productId', productId);
+}
+
+function cancel() {
+    document.getElementById('addBtn').style.display = 'block';
+    document.getElementById('updateAndCancel').style.display = 'none';
+    document.getElementById('productName').value = '';
+    document.getElementById('productPrice').value = '';
+    document.getElementById('productSalePrice').value = '';
+    document.getElementById('productLink').value = '';
+    localStorage.removeItem('productEdit');
+}
+
+function update() {
+    var productId = localStorage.getItem('productId');
+    var products = JSON.parse(localStorage.getItem('products'));
+    for (var key in products) {
+        if (products[key].productId === productId) {
+            var productName = document.getElementById('productName').value;
+            var productPrice = document.getElementById('productPrice').value;
+            var productSalePrice = document.getElementById('productSalePrice').value;
+            var productLink = document.getElementById('productLink').value;
+
+            if (productName.length === 0) {
+                alert('Product name is required');
+                return;
+            }
+
+            if (productPrice.length === 0) {
+                alert('Product price is required');
+                return;
+            }
+
+            if (isNaN(productPrice)) {
+                document.getElementById('productPrice').value = '';
+                alert('Product price must be a number');
+                return;
+            }
+
+            if (productSalePrice.length !== 0) { // nếu như sale price có dữ liệu
+                if (isNaN(productSalePrice)) {
+                    document.getElementById('productSalePrice').value = '';
+                    alert('Product sale price must be a number');
+                    return;
+                }
+
+                if (Number(productSalePrice) > Number(productPrice)) {
+                    document.getElementById('productSalePrice').value = '';
+                    alert('Product sale price must be less than product price');
+                    return;
+                }
+            }
+
+            if (productLink.length === 0) {
+                alert('Product link is required');
+                return;
+            }
+
+            var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+                '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+            if (!pattern.test(productLink)) {
+                document.getElementById('productLink').value = '';
+                alert('Product link is invalid')
+                return;
+            }
+
+            products[key].productName = productName;
+            products[key].productPrice = productPrice;
+            products[key].productSalePrice = productSalePrice;
+            products[key].productLink = productLink;
+        }
+
+        localStorage.setItem('products', JSON.stringify(products));
+        alert('Update Successfully');
         window.location.href = 'admin.html';
     }
 }
